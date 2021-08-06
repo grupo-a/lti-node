38e59f499eb21dbe2ab32dffa16a08fe5e04e84f
Felipe Souza <felipelsouza.dev@gmail.com>
2021-08-06 18:07:47 -0300

    Fix commons bug

--- src/v1.1/helper/outcomeData.js
+++ src/v1.1/helper/outcomeData.js
@@ -1,28 +1,28 @@
 const getOutcomeData = (header) => {
   const paramsList = header.replace(/\*\s|,\s|,/g, '\n');
 
-  let consumerKey = /(oauth_consumer_key)=([^\s]+)/g.exec(paramsList);
+  let consumerKey = /(oauth_consumer_key)="([^\s]+)"/g.exec(paramsList);
   consumerKey = consumerKey && decodeURIComponent(consumerKey[2]);
 
-  let signatureMethod = /(oauth_signature_method)=([^\s]+)/g.exec(paramsList);
+  let signatureMethod = /(oauth_signature_method)="([^\s]+)"/g.exec(paramsList);
   signatureMethod = signatureMethod && decodeURIComponent(signatureMethod[2]);
 
-  let timestamp = /(oauth_timestamp)=([^\s]+)/g.exec(paramsList);
+  let timestamp = /(oauth_timestamp)="([^\s]+)"/g.exec(paramsList);
   timestamp = timestamp && decodeURIComponent(timestamp[2]);
 
-  let nonce = /(oauth_nonce)=([^\s]+)/g.exec(paramsList);
+  let nonce = /(oauth_nonce)="([^\s]+)"/g.exec(paramsList);
   nonce = nonce && decodeURIComponent(nonce[2]);
 
-  let version = /(oauth_version)=([^\s]+)/g.exec(paramsList);
+  let version = /(oauth_version)="([^\s]+)"/g.exec(paramsList);
   version = version && decodeURIComponent(version[2]);
 
-  let bodyHash = /(oauth_body_hash)=([^\s]+)/g.exec(paramsList);
+  let bodyHash = /(oauth_body_hash)="([^\s]+)"/g.exec(paramsList);
   bodyHash = bodyHash && decodeURIComponent(bodyHash[2]);
 
-  let callback = /(oauth_callback)=([^\s]+)/g.exec(paramsList);
+  let callback = /(oauth_callback)="([^\s]+)"/g.exec(paramsList);
   callback = callback && decodeURIComponent(callback[2]);
 
-  let signature = /(oauth_signature)=([^\s]+)/g.exec(paramsList);
+  let signature = /(oauth_signature)="([^\s]+)"/g.exec(paramsList);
   signature = signature && decodeURIComponent(signature[2]);
 
   const params = {
